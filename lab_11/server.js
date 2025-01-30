ddb89866d5a248718b2f5a42acccf12e0898875b
Fraser Barron <f.barron1@rgu.ac.uk>
2025-01-30 10:35:45 +0000

    Add endpoint to serve random knock-knock jokes

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,4 +1,8 @@
 var express = require('express');
+
+const knockKnockJokes = require('knock-knock-jokes');
+var knockJokes = knockKnockJokes();
+
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,6 +11,10 @@ app.get('/', function (req, res) {
 
 app.get('/test', function (req, res) {
     res.send("This is route 2");
+});
+
+app.get('/joke', function (req, res) {
+    res.send(knockJokes);
 });
 
 app.listen(8080);
