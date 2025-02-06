7a4776adc2253def8cbbcdcdc2f2fab80fc0d86a
Fraser Barron <f.barron1@rgu.ac.uk>
2025-02-06 12:00:48 +0000

    Enhance getTopTracks function to generate HTML response for top tracks

--- lab_12/server.js
+++ lab_12/server.js
@@ -88,6 +88,32 @@ async function getTopTracks(artistID, res) {
   spotifyApi.getArtistTopTracks(artistID, "GB").then(
     function (data) {
       console.log(data.body);
+        var tracks = data.body.tracks;
+        var HTMLResponse = "";
+        for (var i = 0; i < tracks.length; i++) {
+          var track = tracks[i];
+          console.log(track.name);
+          HTMLResponse =
+            HTMLResponse +
+            "<div>" +
+            "<h2>" +
+            track.name +
+            "</h2>" +
+            "<h4>" +
+            track.artists[0].name +
+            "</h4>" +
+            "Artist ID: " +
+            track.artists[0].id +
+              "<br>" +
+            "<img src='" +
+            track.album.images[0].url +
+            "'>" +
+            "<a href='" +
+            track.external_urls.spotify +
+            "'> Track Details </a>" +
+            "</div>";
+          console.log(HTMLResponse);
+        }
     },
     function (err) {
       console.log("Something went wrong!", err);
