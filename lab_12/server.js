592ec118b5c25f48db39483815fb96f47e444de3
Fraser Barron <f.barron1@rgu.ac.uk>
2025-02-06 10:53:07 +0000

    Refactor server.js for improved readability and error handling; update access token retrieval and add root route

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,43 +1,50 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var SpotifyWebApi = require('spotify-web-api-node');
-app.use(express.static('public'));
+var SpotifyWebApi = require("spotify-web-api-node");
+app.use(express.static("public"));
 
 var spotifyApi = new SpotifyWebApi({
-    clientId: '7ca02bc758b44ed18a28c5ae66a904c3',
-    clientSecret: '6c26613d5b3346a18c3adf21fae5e4bf'
-    });
+  clientId: "7ca02bc758b44ed18a28c5ae66a904c3",
+  clientSecret: "6c26613d5b3346a18c3adf21fae5e4bf",
+});
 
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant().then(
-    function(data){
-        console.log("The access token expires in " + data.body['expires_in']);
-        console.log("The access token is " + data.body['access_token']);
+  function (data) {
+    console.log("The access token expires in " + data.body["expires_in"]);
+    console.log("The access token is " + data.body["access_token"]);
 
-        // save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body['access_token']);
+    // save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body["access_token"]);
+  },
+
+  function (err) {
+    console.log(
+      "Something went wrong when retrieving an access token",
+      err.message
+    );
+  }
+);
+
+async function getTracks(searchterm, res) {
+  spotifyApi.searchTracks(searchterm).then(
+    function (data) {
+      res.send(JSON.stringify(data.body));
     },
-
-    function(err){
-        console.log("Something went wrong when retrieving an access token", err.message);
-    });
-
-async function getTracks(searchterm, res){
-    spotifyApi.searchTracks(searchterm)
-    .then(function(data){
-        res.send(JSON.stringify(data.body));
-    }, function(err){
-        console.error(err);
-    });
+    function (err) {
+      console.error(err);
+    }
+  );
 }
 
-// route for love in tracks, artists and albums
-app.get('/searchLove', function(req, res){
-    getTracks('love', res);
+
+app.get("/", function (req, res) {
+  res.send("Hello World! By Express");
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World! By Express');
-    });
+// route for love in tracks, artists and albums
+app.get("/searchLove", function (req, res) {
+    getTracks("love", res);
+  });
 
 app.listen(8080);
